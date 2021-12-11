630619ee3399734fb0b017a4cc32a21ed967e217
Gerardo Garzón <gerardogv20@gmail.com>
2021-12-11 17:54:27 -0600

    Added comments

--- dumbo_octopus.py
+++ dumbo_octopus.py
@@ -9,11 +9,19 @@ def increase_by_one(x):
 
 
 def increase_energy_of_adjacent_octupuses(grid: list, position: tuple) -> list:
+    """Increases the energy level for each octupus adjacent to one that flashed.
+    For each octopus: 10 means: flashing octopus that hasn't yet increased the
+    energy of the adjacent octopuses. 11 means: flashing octopus that has already
+    increased the energy of the adjacent octopuses.
+
+    Args:
+        grid (list): [Energy level and position of each octopus]
+        position (tuple): [Position of the flashing octopus]
+
+    Returns:
+        list: [Updated grid]
     """
-    (x-1, y-1)  (x-1, y)    (x-1, y+1)
-    (x, y-1)    (x, y)      (x, y+1)
-    (x+1, y-1)  (x+1, y)    (x+1, y+1)
-    """
+
     x = position[0]
     y = position[1]
 
@@ -49,6 +57,15 @@ def increase_energy_of_adjacent_octupuses(grid: list, position: tuple) -> list:
 
 
 def count_flashes_and_adjust_energy_levels(grid):
+    """Counts the number of flashes and resets the energy levels for
+    each octopus that flashed in the round.
+
+    Args:
+        grid ([type]): [Energy level and position of each octopus]
+
+    Returns:
+        [type]: [description]
+    """
     flashes = 0
 
     # Starts at 1, ends at -1, to skip helper cells
@@ -62,6 +79,17 @@ def count_flashes_and_adjust_energy_levels(grid):
 
 
 def get_number_of_flashes(grid: list, steps: int, until_sync=False) -> int:
+    """Calculate the total number of flashes emitted by the octupuses inside the cave.
+
+    Args:
+        grid (list): [Energy level and location of each octopus]
+        steps (int): [Number of steps to be simulated]
+        until_sync (bool, optional): [Special flag to indicate if we want the number of the
+        first step when all the octupuses flash at the same time.]. Defaults to False.
+
+    Returns:
+        int: [Number of flashes (or number of first synchronization if requested)]
+    """
 
     flashes = 0
     start_steps = steps
@@ -94,7 +122,7 @@ def get_number_of_flashes(grid: list, steps: int, until_sync=False) -> int:
         grid = step_results[0]
         flashes += step_results[1]
 
-        if step_results[1] == (len(grid) - 2) * (len(grid[1]) - 2):
+        if step_results[1] == (len(grid) - 2) * (len(grid[1]) - 2) and until_sync:
             return start_steps - steps + 1
 
         steps -= 1
